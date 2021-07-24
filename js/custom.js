1d20f20e80af672b0194ea0c18eed2e1d6645b8a
venky <venkatesh20172018@gail.com>
2021-07-24 13:35:36 +0530

    first push

---
+++ js/custom.js
@@ -0,0 +1,142 @@
+/*  Theme Name: Oflox - Responsive Multipurpose Business Template
+    Author: Saptavarana
+    Version: 1.0.0
+    Created:September 2018
+    File Description:Main Js file of the template
+*/
+
+(function($) {
+
+    'use strict';
+
+    function initNavbarStickey() {
+        $(window).on('scroll', function() {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 50) {
+                $(".sticky").addClass("stickyadd");
+            } else {
+                $(".sticky").removeClass("stickyadd");
+            }
+        });
+    }
+
+    function initSmoothLink() {
+        $('.navbar-nav a, .scroll_down a').on('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 0
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    }
+
+    function initScrollspy() {
+        $("#navbarCollapse").scrollspy({
+            offset: 20
+        });
+    }
+
+    function initTesti() {
+        $("#testi").owlCarousel({
+            autoPlay: 28000,
+            items: 1,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [979, 3]
+        });
+    }
+
+    function initMfpvideo() {
+        $('.img-zoom').magnificPopup({
+            type: 'image',
+            closeOnContentClick: true,
+            mainClass: 'mfp-fade',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1]
+            }
+        });
+
+        $('.features_video').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+    }
+
+    function initPortfolio() {
+        $(window).on('load', function() {
+            var $container = $('.work-filter');
+            var $filter = $('#menu-filter');
+            $container.isotope({
+                filter: '*',
+                layoutMode: 'masonry',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear'
+                }
+            });
+
+            $filter.find('a').on("click", function() {
+                var selector = $(this).attr('data-filter');
+                $filter.find('a').removeClass('active');
+                $(this).addClass('active');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        animationDuration: 750,
+                        easing: 'linear',
+                        queue: false,
+                    }
+                });
+                return false;
+            });
+        });
+    }
+
+    function initCounter(){
+        var a = 0;
+        $(window).on('scroll', function() {
+            var oTop = $('#counter').offset().top - window.innerHeight;
+            if (a == 0 && $(window).scrollTop() > oTop) {
+                $('.counter_value').each(function() {
+                    var $this = $(this),
+                        countTo = $this.attr('data-count');
+                    $({
+                        countNum: $this.text()
+                    }).animate({
+                        countNum: countTo
+                    }, {
+                        duration: 2000,
+                        easing: 'swing',
+                        step: function() {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function() {
+                            $this.text(this.countNum);
+                            //alert('finished');
+                        }
+
+                    });
+                });
+                a = 1;
+            }
+        });
+    }
+
+    function init() {
+        initNavbarStickey();
+        initSmoothLink();
+        initScrollspy();
+        initTesti();
+        initMfpvideo();
+        initPortfolio();
+        initCounter();
+    }
+    init();
+
+})(jQuery)
